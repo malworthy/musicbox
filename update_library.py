c26737db2e401d401a8e8afbe3aa6a7ffc93d8c5
malcolm worthy <malworthy@gmail.com>
2023-01-24 20:26:36 +1100

    add option to only add new songs to library

--- update_library.py
+++ update_library.py
@@ -4,6 +4,44 @@ import music_tag
 import glob
 import sqlite3
 import json
+import sys
+
+def get_files():
+    print("Retreiving files")
+    f = open("config.json")
+    config = json.load(f)
+    f.close()
+
+    return glob.glob(f"{config['library']}**/*.mp3", recursive=True)
+
+def extract_tags(full_refresh):
+    data = []
+    print("Extracting tags")
+
+    for file in get_files():
+        try:
+            print(file)      
+            if full_refresh or is_file_in_library(file) == False:  
+                tags = music_tag.load_file(file)
+                data.append( (file, str(tags["tracktitle"]), str(tags["artist"]), str(tags["album"]), str(tags["albumartist"]),int(tags["tracknumber"]),int(tags["#length"])) )
+        except:
+            print("Error, could not add this file")
+    add_to_database(data, full_refresh)
+
+def is_file_in_library(filename):
+    cur.execute("select count(*) from library where filename = ?",(filename,))
+    result = cur.fetchone()
+    return result[0] > 0
+
+def add_to_database(data, full_refresh):
+    print("Inserting records into database")
+    if full_refresh:
+        cur.execute("delete from library")
+    cur.executemany("insert into library(filename, tracktitle, artist, album, albumartist, tracknumber, length) values(?,?,?,?,?,?,?)", data)
+    cur.execute("update library set albumartist = artist where albumartist = ''")
+    con.commit()
+
+## main entry point ##
 
 con = sqlite3.connect("musiclibrary.db")
 cur = con.cursor()
@@ -13,27 +51,9 @@ try:
 except:
     pass
 
-print("Retreiving files")
-data = []
-f = open("config.json")
-config = json.load(f)
-f.close()
+full_refresh = True
+if len(sys.argv) > 1:
+    full_refresh = sys.argv[1] == "-a" or sys.argv[1] == "--add"
 
-files = glob.glob(f"{config['library']}**/*.mp3", recursive=True)
-print("Extracting tags")
-
-for file in files:
-    try:
-        print(file)        
-        tags = music_tag.load_file(file)
-        data.append( (file, str(tags["tracktitle"]), str(tags["artist"]), str(tags["album"]), str(tags["albumartist"]),int(tags["tracknumber"]),int(tags["#length"])) )
-    except:
-        print("Error, could not add this file")
-
-print("Inserting records into database")
-cur.execute("delete from library")
-cur.executemany("insert into library(filename, tracktitle, artist, album, albumartist, tracknumber, length) values(?,?,?,?,?,?,?)", data)
-cur.execute("update library set albumartist = artist where albumartist = ''")
-con.commit()
-
+extract_tags(full_refresh)
 print("finished")
